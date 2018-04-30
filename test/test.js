e053670d40961123b00941e24950091d91746163
Hannah Bunce <hannahlbunce@gmail.com>
2018-04-30 19:40:27 +0100

    Tests around index.html and about.html

--- test/test.js
+++ test/test.js
@@ -4,10 +4,33 @@ var assert = require('assert')
 
 describe('Calisaurus boardgames website', function() {
   describe('Index Page', function() {
-    it('should contain links to other pages', function() {
+    let actual
+    beforeEach(function() {
       return fetch('http://boardgames.calisaurus.net').then(function(body) {
-        expect(body).to.have.string('<a href="./about.html">About A Board Game A Day</a>')
+        actual = body
       })
+    })
+    it('should contain links to other pages', function() {
+      expect(actual).to.have.string('<a href="./about.html">About A Board Game A Day</a>')
+    })
+
+    it('should contain an image', function() {
+      expect(actual).to.have.string('calisaurus.png')
+    })
+  })
+
+  describe('About page', function() {
+    let actual
+    beforeEach(function() {
+      return fetch('http://boardgames.calisaurus.net/about').then(function(body) {
+        actual = body
+      })
+    })
+    it('should contain a heading', function() {
+      expect(actual).to.have.string('<h1>About</h1>')
+    })
+    it('should contain an image', function() {
+      expect(actual).to.have.string('about.jpg')
     })
   })
 })
