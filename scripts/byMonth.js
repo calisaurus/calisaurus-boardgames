ee0a12007c02f98e6af7b0b8df219472509e318f
Cali <hannahlbunce@gmail.com>
2020-03-23 21:51:47 +0000

    Refactor vue app name for monthly stats

---
+++ scripts/byMonth.js
@@ -0,0 +1,41 @@
+new Vue({
+  el: '#byMonthApp',
+  data () {
+    return {
+      info: {}
+    }
+  },
+  props: ['datecode'],
+  mounted () {
+    const dateCode = document.getElementById('dateCode').getAttribute('data-dateCode')
+    axios
+      .get(`https://boardgames-api.calisaurus.net/api/boardgame/stats/byMonth/${dateCode}`)
+      .then(response => (this.info = response))
+  },
+  computed: {
+      monthStats: function() {
+          return this.info.data
+      },
+      mostGamesPlayedInADay: function() {
+          return this.sortByDate(this.monthStats.mostGamesPlayedInADay)
+      }
+  },
+  methods: {
+      formatPercentage: function(number) {
+          return Number(number * 100).toPrecision(2) + '%'
+      },
+      formatDate: function(date) {
+          return moment(date).format('Do MMMM')
+      },
+      formatAverage: function(number) {
+        return Number(number).toPrecision(2)
+      },
+      sortByDate: function(list) {
+        return list.sort(function (a, b) {
+            const dateA = new Date(a.date)
+            const dateB = new Date(b.date)
+            return dateA - dateB
+        })
+      }
+  }
+})
